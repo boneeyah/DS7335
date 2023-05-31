1a4e56b99ae82745fbbed48bc03abc988d213d00
boneeyah <migbonm@gmail.com>
2023-05-30 22:36:38 -0400

    add csv file for data

---
+++ HM2.py
@@ -0,0 +1,65 @@
+# Homework 2
+import numpy as np
+from sklearn.metrics import accuracy_score # other metrics too pls!
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier # more!
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
+from itertools import product
+import matplotlib.pyplot as plt
+
+# adapt this code below to run your analysis
+# 1. Write a function to take a list or dictionary of clfs and hypers(i.e. use logistic regression), each with 3 different sets of hyper parameters for each
+# 2. Expand to include larger number of classifiers and hyperparameter settings
+# 3. Find some simple data
+# 4. generate matplotlib plots that will assist in identifying the optimal clf and parampters settings
+# 5. Please set up your code to be run and save the results to the directory that its executed from
+# 6. Investigate grid search function
+
+M = np.loadtxt('heart.csv',delimiter=",",skiprows=1,usecols=range(0,13))
+L = np.loadtxt('heart.csv',delimiter=",",skiprows=1,usecols=13)
+n_folds = 5
+
+data = (M, L, n_folds)
+
+def run(clf_dict, data):
+    M, L, n_folds = data # unpack data container
+    kf = KFold(n_splits=n_folds) # Establish the cross validation
+    ret = {} ####### switch back to dictionary of dictionaries # classic explication of results
+    idx = 0
+    for key in clf_dict.keys():
+        for ids, (train_index, test_index) in enumerate(kf.split(M, L)):
+        # will take the product of the hyperparameter dictionary
+            prods = list(product(*clf_dict.get(key).values()))
+        # run for each possible product of the hyperparameters building a new dictionary and updating
+            for i in prods:
+                a_dict = dict(zip(clf_dict.get(key).keys(),i))
+                clf = key(**a_dict)
+                clf.fit(M[train_index], L[train_index])
+                pred = clf.predict(M[test_index])
+                ret.update({idx:{'clf':key,
+                                 'model':clf,
+                                 'train_index': train_index,
+                                 'test_index': test_index,
+                                 'accuracy':accuracy_score(L[test_index],pred)}})
+                idx +=1
+    return ret
+
+a_dict = {RandomForestClassifier:{'n_estimators':[10,100,1000],'max_depth':[1000,None],'max_features':['sqrt','log2']},
+          LogisticRegression:{'penalty':['l1','l2'],'C':[.1,1,10],'solver':['liblinear','saga'],'max_iter':[6000]},
+          AdaBoostClassifier:{'learning_rate':[.5,1,2],'n_estimators':[25,50,100]}}
+results = run(a_dict, data)
+#print(results)
+
+X = []
+y = []
+for i in results.keys():
+    scores = results.get(i).get('accuracy')
+    y.append(scores)
+    X.append(i)
+print(X,y)
+
+
+
+
+
+
